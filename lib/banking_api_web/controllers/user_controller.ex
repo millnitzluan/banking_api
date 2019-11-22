83cb1bf4812666647db74f879d8284242d14100c
millnitzluan <luanmillnitz@gmail.com>
2019-11-22 18:00:00 -0300

    add test user_controller_test

--- lib/banking_api_web/controllers/user_controller.ex
+++ lib/banking_api_web/controllers/user_controller.ex
@@ -7,11 +7,6 @@ defmodule BankingApiWeb.UserController do
 
   action_fallback BankingApiWeb.FallbackController
 
-  def index(conn, _params) do
-    users = Account.list_users()
-    render(conn, "index.json", users: users)
-  end
-
   def create(conn, %{"user" => user_params}) do
     with {:ok, %User{} = user} <- Account.create_user(user_params),
          {:ok, token, _claims} <- Guardian.encode_and_sign(user) do
@@ -22,29 +17,15 @@ defmodule BankingApiWeb.UserController do
 
   def show(conn, _params) do
     user = Guardian.Plug.current_resource(conn)
-    conn |> render("user.json", user: user)
-  end
-
-  def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Account.get_user!(id)
-
-    with {:ok, %User{} = user} <- Account.update_user(user, user_params) do
-      render(conn, "show.json", user: user)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    user = Account.get_user!(id)
-
-    with {:ok, %User{}} <- Account.delete_user(user) do
-      send_resp(conn, :no_content, "")
-    end
+    conn
+    |> render("user.json", user: user)
   end
 
   def sign_in(conn, %{"email" => email, "password" => password}) do
     case Account.token_sign_in(email, password) do
       {:ok, token, _claims} ->
-        conn |> render("jwt.json", jwt: token)
+        conn
+        |> render("jwt.json", jwt: token)
       _ ->
         {:error, :unauthorized}
     end
